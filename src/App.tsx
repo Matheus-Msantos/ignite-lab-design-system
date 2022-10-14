e92ed632ac53529476180b85ca425840f144eb93
Matheus-Msantos <matheusmelosantos10@gmail.com>
2022-10-14 19:48:56 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './style/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl">Hello world</h1>
+      <button className="bg-cyan-500">Click</button>
+    </>
+  )
+}
