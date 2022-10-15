580334694efd61babf8e4e0f51d6bbb7d090488a
Matheus-Msantos <matheusmelosantos10@gmail.com>
2022-10-14 21:17:23 -0300

    Add page static login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,56 @@
+import { Envelope, Lock } from 'phosphor-react';
+import { Button } from './components/Button';
+import { Heading } from './components/Heading';
+import { Text } from './components/Text';
+import { TextInput } from './components/TextInput';
+import { Logo } from './Logo';
 import './style/global.css';
 
 export function App() {
   return (
-    <>
-      <h1 className="font-bold text-2xl">Hello world</h1>
-      <button className="bg-cyan-500">Click</button>
-    </>
+    <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center">
+      <header className="flex flex-col items-center">
+        <Logo />
+
+        <Heading size="lg" className="mt-4">
+          Ignite Lab
+        </Heading>
+
+        <Text size="lg" className="text-gray-400 mt-1">Faça login e comece a usar!</Text>
+      </header>
+
+      <form className="flex flex-col gap-4 items-stretch w-full max-w-[400px] mt-10">
+        <label htmlFor="email" className="flex flex-col gap-3">
+          <Text>Endereço de e-mail</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input id="email" type="email" placeholder="Digite seu e-mail" />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="password" className="flex flex-col gap-3">
+          <Text>Sua senha</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+            <TextInput.Input id="password" type="password" placeholder="*********" />
+          </TextInput.Root>
+        </label>
+
+        <Button type="submit" className='mt-4'>Entrar na plataforma</Button>
+      </form>
+
+      <footer className="flex flex-col items-center gap-4 mt-8">
+        <Text asChild size="sm">
+          <a href="#" className="text-gray-400 underline hover:text-gray-200">Esqueceu sua senha?</a>
+        </Text>
+        <Text asChild size="sm">
+          <a href="#" className="text-gray-400 underline hover:text-gray-200">Não possui conta? Crie uma agora!</a>
+        </Text>
+      </footer>
+    </div>
   )
 }
